26bc2fdbab31eef32ed4d695e830904e90035b6f
Aureche16 <aurelien1612@orange.fr>
2019-12-11 13:52:55 +0100

    Module arbre

--- arbre.c
+++ arbre.c
@@ -1,21 +1,108 @@
-//Algo de tri par insertion
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "arbre.h"
+#define T_MAX 100
 
+int est_arbre_vide(arbre a){
+    return (a==NULL) ;
+}
 
-void insertion (liste , arbre) {
-	
+arbre gauche (arbre a){
+    return a->gauche;
+}
+
+arbre droit (arbre a){
+    return a->droit;
+}
+
+int poids(arbre a){
+    return a->poids;
+}
+
+char carac(arbre a){
+    return a->carac;
+}
+
+arbre creer_arbre(void){
+    return malloc(sizeof(noeud));
+}
+
+arbre ajoute_gauche(arbre a){
+    a->gauche = creer_arbre() ;
+    return gauche(a);
+}
+
+arbre ajoute_droit(arbre a){
+    a->droit = creer_arbre();
+    return droit(a);
+}
+
+void set_poids (arbre a , int pds){
+    a->poids = pds ;
+}
+
+void set_carac(arbre a, char c){
+    a->carac = c ;
+}
+
+int max (int a, int b){
+    if (a>b) return a ;
+    else return b ;
+}
+
+// calcule la hauteur d'un arbre
+int hauteur(arbre a){
+    if(est_arbre_vide(a)) return 0 ;
+    else {
+        return 1 + max(hauteur(a->gauche), hauteur(a->droit)) ;
+    }
 }
 
 
+char* serialisation_plus(arbre a){
+    char *str;
+    int i;
+    str = malloc(sizeof(char) * T_MAX);
+    i = 0;
+    int* p_i = &i;
+    serialisation(a,str,p_i);
+    str[i] = 0;
+    return str;
+}
 
+void serialisation(arbre a, char*str, int*i){
+    if (a==NULL){
+        str[*i] = '0';
+        (*i)++;
+    }else{
+        str[*i] = '1';
+        (*i)++;
+        str[*i] = a->carac;
+        (*i)++;
+        serialisation(a->gauche, str, i);
+        serialisation(a->droit, str, i);
+    }
+}
 
-void afficher(int tab[], int taille)
-{
-	printf("[ ");
-	for (int i = 0; i < taille; i++) printf("%d ", tab[i]) ;
-	printf("]\n") ;
-	return ; 
+arbre deserialisation_sup(char *chaine){
+    int i;
+    i=0;
+    return deserialisation(chaine,&i);
+}
+
+arbre deserialisation (char* chaine, int*i){
+    arbre pointeur ;
+    if (chaine[*i]=='0'){
+        (*i)++;
+        pointeur = NULL;
+    } else if (chaine[*i]=='1'){
+        (*i)++;
+        arbre pointeur = creer_arbre();
+        set_poids(pointeur,chaine[*i]);
+        (*i)++;
+        pointeur->gauche = (deserialisation(chaine,i));
+        pointeur->droit = (deserialisation(chaine,i));
+        //return pointeur;
+    }
+    return pointeur;
 }
--- arbre.h
+++ arbre.h
@@ -1,4 +1,73 @@
-#ifndef ARBRE_H
-#define ARBRE_H
-    void insertion(liste, arbre);
+#ifndef _ARBRE_H_
+#define _ARBRE_H_
+
+typedef struct t_noeud
+{
+    struct t_noeud *droit ;
+    struct t_noeud *gauche;
+    int poids ;
+    char carac ;
+}noeud;
+typedef noeud* arbre ;
+
+typedef struct t_element 
+{
+    arbre pArbre ;
+    struct t_element *suiv;
+    struct t_element *precedent;
+}element;
+
+typedef element* liste ;
+
+// Vérifie si un arbre est vide
+int est_arbre_vide(arbre a); 
+
+// Retourne l'adresse de la branche a gauche
+arbre gauche(arbre a);
+
+// Retourne l'adresse de la branche a droite
+arbre droit(arbre a);
+
+// Retourne le poids du noeud de a
+int poids(arbre a);
+
+// Retourne le caractere du noeud de a
+char carac(arbre a);
+
+// Rencoie un pointeur sur un arbre vide
+arbre creer_arbre(void);
+
+// Ajoute un arbre a gauche du neoud a
+arbre ajoute_gauche(arbre a);
+
+// Ajoute un arbre a droite du neoud a
+arbre ajoute_droit(arbre a);
+
+// Fixe le poids du noeud
+void set_poids(arbre a, int poids);
+
+// Fixe le caractere d'un noeud
+void set_carac(arbre a, char c);
+
+// Transforme un arbre en chaine de caractere
+void serialisation(arbre a, char *str, int* i);
+
+// Transforme une str en arbre
+arbre deserialisation(char *chaine, int *i);
+
+// Calcule le max entre 2 entiers
+int max (int a, int b);
+
+// Calcule la hauteur d'un arbre
+int hauteur(arbre a);
+
+// Effectue la serialisation d'un arbre
+char* serialisation_plus(arbre a);
+void serialisation(arbre a, char*str, int*i);
+
+// Effectue la deserialisation d'un arbre
+arbre deserialisation_sup(char *chaine);
+arbre deserialisation (char* chaine, int*i);
+
+
 #endif
