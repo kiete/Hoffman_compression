2e896214321b9d6bb2f960c98787fa7b0931fa5c
Antoche <delolme.antonin07@orange.fr>
2019-12-13 10:40:55 +0100

    Type entrée défini

--- codage.h
+++ codage.h
@@ -2,17 +2,22 @@
 #define CODAGE_H
 #include "arbre.h"
 
-typedef struct dico_t
+typedef struct entree_t
 {
     char c;
     char* seqBits;
     int len;
-} dico ;
+} entree ;
+
+typedef entree* dico;
 
 // etablit la correspondance d'un char avec sa sequence de bits
-void codage_dico(arbre a, dico table[], int MaxCar);
+void codage_dico(arbre a, dico, int MaxCar);
 
-// 
+// Construit un tableau de dico
+dico get_table(arbre a);
 
+// Decode le caractere c par le dico d.
+void decode(char c, dico d);
 
 #endif
