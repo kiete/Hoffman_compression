d18b6b73541ae159ebe0762db82df4014b730baa
Antoche <delolme.antonin07@orange.fr>
2019-12-13 10:09:20 +0100

    Codage MAJ

--- caractere.h
+++ caractere.h
@@ -1,22 +1,7 @@
 #ifndef CARACTERE_H
 #define CARACTERE_H
 
+#define OCTET 8
 
-// test
-typedef struct t_noeud
-{
-    struct t_noeud* droit ;
-    struct t_noeud *gauche;
-    int poid ;
-    char carac ;
-}noeud;
-typedef noeud* arbre ;
-
-typedef struct t_element 
-{
-    arbre pArbre ;
-    struct t_element *suiv;
-}element;
-typedef element* liste ;
 
 #endif
