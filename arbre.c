e2e5806fce065aa00bac35d5dcc739de09c3d3f1
MrTheoker <tkerneves@gmail.com>
2019-12-15 15:10:44 +0100

    Add files via upload

--- arbre.c
+++ arbre.c
@@ -19,7 +19,7 @@ int poids(arbre a){
     return a->poids;
 }
 
-unsigned char carac(arbre a){
+char carac(arbre a){
     return a->carac;
 }
 
@@ -66,6 +66,11 @@ int est_feuille (arbre a) {
     return (est_arbre_vide(a->gauche) && est_arbre_vide(a->droit)) ;
 }
 
+int max (int a, int b){
+    if (a>b) return a ;
+    else return b ;
+}
+
 // calcule la hauteur d'un arbre
 int hauteur(arbre a){
     if(est_arbre_vide(a)) return 0 ;
@@ -97,10 +102,10 @@ void print_arbre(arbre a) // Notation préfixe
 }
 
 
-unsigned char* serialisation_plus(arbre a){
+char* serialisation_plus(arbre a){
     char *str;
     int i;
-    str = malloc(sizeof(unsigned char) * T_MAX);
+    str = malloc(sizeof(char) * T_MAX);
     i = 0;
     int* p_i = &i;
     serialisation(a,str,p_i);
@@ -108,28 +113,27 @@ unsigned char* serialisation_plus(arbre a){
     return str;
 }
 
-void serialisation(arbre a, unsigned char*str, int*i){
+void serialisation(arbre a, char*str, int*i){
     if (a==NULL){
         str[*i] = '0';
         (*i)++;
     }else{
         str[*i] = '1';
         (*i)++;
-        if (a->carac == 0) str[*i] = 'O';
-        else str[*i] = a->carac ;
+        str[*i] = a->carac;
         (*i)++;
         serialisation(a->gauche, str, i);
         serialisation(a->droit, str, i);
     }
 }
 
-arbre deserialisation_sup(unsigned char *chaine){
+arbre deserialisation_sup(char *chaine){
     int i;
     i=0;
     return deserialisation(chaine,&i);
 }
 
-arbre deserialisation (unsigned char* chaine, int*i){
+arbre deserialisation (char* chaine, int*i){
     arbre pointeur ;
     if (chaine[*i]=='0'){
         (*i)++;
